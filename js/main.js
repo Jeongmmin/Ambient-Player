81eeed96d5925164e1584c19ef6958980aaf8dad
Jeongmmin <seo88kr@hanmail.net>
2022-10-01 23:52:43 +0900

    Add music file, Add js css

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-const img = document.querySelector('img');
+const btn = document.querySelector('.buttons__preBtn');
 
+const music1 = new Audio('asset/sound/ghost2.mp3');
+
+let isPlaying = false;
+
+btn.addEventListener('click', () => {
+  playMusic(music1);
+});
+
+function playMusic(songName) {
+  isPlaying = !isPlaying;
+  isPlaying ? songName.pause() : songName.play();
+}
